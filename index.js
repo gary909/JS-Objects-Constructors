a55b774662806435550e2b31f938a5c54e475042
gary <gary.white@lodgit.com>
2021-11-30 16:31:37 +0100

    stuff

---
+++ index.js
@@ -0,0 +1,21 @@
+//JS Object:
+// const myCar = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1969
+// };
+
+//JS Constructor function
+// function should be capitalised
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+var mycar1 = new Car('Eagle', 'Talon TSi', 1993);
+
+// console.log(myCar.year);
+// console.log(myCar.model);
+console.log(mycar1.year);
+console.log(mycar1.model);
